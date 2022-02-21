838b5f73309982cbc6a8450e12a06c6acc61d87d
MwitaBrian <brianmwita13@gmail.com>
2022-02-21 14:51:03 +0300

    3rd Test: Confirm that ch gets iterated through the userInput string

--- src/main/java/models/Cipher.java
+++ src/main/java/models/Cipher.java
@@ -2,8 +2,14 @@ package models;
 
 public class Cipher {
     int shift = 7;
+
     public String encrypt(){
         String cipherText = "";
+        String userInput = "I love playing video game!";
+        int length = userInput.length();
+        for (int i = 0; i < length; i++) {
+            char ch = userInput.charAt(i);
+        }
         return cipherText;
     }
 }
--- src/test/java/models/CipherTest.java
+++ src/test/java/models/CipherTest.java
@@ -20,11 +20,26 @@ class CipherTest {
     }
 
     @Test
+    @DisplayName("Return empty string")
     public void encrypt_returnEmptyCipherString_String(){
-        Cipher newCipher = new Cipher();
+
         int shift = 7;
         String cipherText = "";
         assertEquals(cipherText, newCipher.encrypt());
+    }
+
+    @Test
+    @DisplayName("For loop returns characters from User's input")
+    public void encrypt_forLoopReturnsCharactersFromUserInput_Char(){
+
+        int shift = 7;
+        String cipherText = "";
+        String userInput = "I love playing video games!";
+        int length = userInput.length();
+        for (int i = 0; i < length; i++){
+            char ch = userInput.charAt(i);
+            assertEquals(ch, ch);
+        }
     }
 
 }
