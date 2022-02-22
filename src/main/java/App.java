8634778a8ab1ff3ed6654f6d03c44484c465ed3a
MwitaBrian <brianmwita13@gmail.com>
2022-02-22 16:50:53 +0300

    Confirm encrypt Function works. Returns encrypted String

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,17 @@
+import models.Cipher;
+import java.util.Scanner;
+
 public class App {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Cipher newCipher = new Cipher();
+
+        System.out.println("Welcome to Caesar Cipher.");
+        System.out.println("Please enter the text you would like to Encrypt.");
+
+        String userInput = input.nextLine();
+
+        String cipheredText = newCipher.encrypt(userInput);
+        System.out.println(cipheredText);
+    }
 }
